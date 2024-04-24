f5e10f2f790269f701b19d0c7ee94317127a8f87
Jenniffer Tenempaguay <jenntenem@gmail.com>
2024-04-24 17:23:06 -0500

    contains

--- Program.cs
+++ Program.cs
@@ -28,13 +28,17 @@ var books = linqData.getAllBooks();
 // );
 
 // * All - ANY
-// bool _allBooksHaveStatus = books.All(b => b.Status != string.Empty || b.Status != null);
-bool _allBooksHaveStatus = books.All(b => !string.IsNullOrWhiteSpace(b.Status));
-bool _anyBooksHaveStatus = books.Any(b => b.Status == string.Empty || b.Status == null);
+// // bool _allBooksHaveStatus = books.All(b => b.Status != string.Empty || b.Status != null);
+// bool _allBooksHaveStatus = books.All(b => !string.IsNullOrWhiteSpace(b.Status));
+// bool _anyBooksHaveStatus = books.Any(b => b.Status == string.Empty || b.Status == null);
 
-Console.WriteLine($"Todos los libros tienen status: {_allBooksHaveStatus}");
-Console.WriteLine($"Existe un libro que no tenga status: {_anyBooksHaveStatus}");
+// Console.WriteLine($"Todos los libros tienen status: {_allBooksHaveStatus}");
+// Console.WriteLine($"Existe un libro que no tenga status: {_anyBooksHaveStatus}");
 
-// Any Book published in 2005
-bool _anyBook2005 = books.Any(b => b.PublishedDate.Year == 2005);
-Console.WriteLine($"Existe un libro publicado en 2005: {_anyBook2005}");
+// // Any Book published in 2005
+// bool _anyBook2005 = books.Any(b => b.PublishedDate.Year == 2005);
+// Console.WriteLine($"Existe un libro publicado en 2005: {_anyBook2005}");
+
+// * CONTAINS
+linqData.printValues(books.Where(b => b.Title.Contains("Python"))); // string -> ike
+linqData.printValues(books.Where(b => b.Categories.Contains("Python"))); // string[] -> include/where
