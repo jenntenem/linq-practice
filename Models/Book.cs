457db5f8a2f9167becbc3a7d955d60afd40e6d39
Jenniffer Tenempaguay <jenntenem@gmail.com>
2024-04-24 00:07:44 -0500

    fix variable declaration like string

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,6 +1,6 @@
 class Book {
   public string Title { get; set; }
-  public string PageCount { get; set; }
+  public int PageCount { get; set; }
   public DateTime PublishedDate { get; set; }
   public string Status { get; set; }
   public string[] Authors { get; set; }
